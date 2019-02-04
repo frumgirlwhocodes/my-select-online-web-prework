28ae2a3ab12dfc04e3e5ae9904b56dcbc728b160
lieby weintraub <liebahinda@gmail.com>
2019-02-04 01:25:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,14 @@
-def my_select(collection)
+ef my_select(collection)
+	i = 0
+	selection = []
+	if collection.length > 0
+		while i < collection.length
+			if yield(collection[i]) == true
+			    selection << collection[i]
+			end
+			i+=1
+		end
+	end
+	selection
  # your code here!
- collection.select { |num|  num.even?  }
 end
